c14e0f86610d81bf630aee3ae80204e7419979c8
jose_jesus_guzman <jose1824@live.com.mx>
2016-08-03 23:17:59 -0500

    No probado

--- app/src/main/java/com/example/pc_/contactos/MainActivity.java
+++ app/src/main/java/com/example/pc_/contactos/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.pc_.contactos;
 
+import android.content.ContentResolver;
+import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
@@ -7,12 +9,158 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.SparseBooleanArray;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
+    List<String> name1 = new ArrayList<String>();
+    List<String> phno1 = new ArrayList<String>();
+    MyAdapter ma ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getAllContacts(this.getContentResolver());
+        ListView lv= (ListView) findViewById(R.id.list);
+        ma = new MyAdapter();
+        lv.setAdapter(ma);
+        lv.setOnItemClickListener(this);
+        lv.setItemsCanFocus(false);
+        lv.setTextFilterEnabled(true);
+        // adding
+        /*
+        select = (Button) findViewById(R.id.button1);
+        select.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v) {
+                StringBuilder checkedcontacts= new StringBuilder();
+                System.out.println(".............."+ma.mCheckStates.size());
+                for(int i = 0; i < name1.size(); i++)
+
+                {
+                    if(ma.mCheckStates.get(i)==true)
+                    {
+                        checkedcontacts.append(name1.get(i).toString());
+                        checkedcontacts.append("\n");
+
+                    }
+                    else
+                    {
+                        System.out.println("Not Checked......"+name1.get(i).toString());
+                    }
+
+
+                }
+
+                Toast.makeText(MainActivity.this, checkedcontacts,Toast.LENGTH_SHORT).show();
+            }
+        });
+*/
+
+    }
+    @Override
+    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+        // TODO Auto-generated method stub
+        ma.toggle(arg2);
+    }
+
+    public  void getAllContacts(ContentResolver cr) {
+
+        Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
+        while (phones.moveToNext())
+        {
+            String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            System.out.println(".................."+phoneNumber);
+            name1.add(name);
+            phno1.add(phoneNumber);
+        }
+
+        phones.close();
+    }
+    class MyAdapter extends BaseAdapter implements CompoundButton.OnCheckedChangeListener
+    {  private SparseBooleanArray mCheckStates;
+        LayoutInflater mInflater;
+        TextView tv1,tv;
+        MyAdapter()
+        {
+            mCheckStates = new SparseBooleanArray(name1.size());
+            mInflater = (LayoutInflater)MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return name1.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+
+            return 0;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            // TODO Auto-generated method stub
+            View vi=convertView;
+            if(convertView==null)
+                vi = mInflater.inflate(R.layout.item_contact, null);
+            TextView tv= (TextView) vi.findViewById(R.id.item_txt_nombre);
+            tv1= (TextView) vi.findViewById(R.id.item_txt_telefono);
+            tv.setText("Name :"+ name1.get(position));
+            tv1.setText("Phone No :"+ phno1.get(position));
+            return vi;
+        }
+        public boolean isChecked(int position) {
+            return mCheckStates.get(position, false);
+        }
+
+        public void setChecked(int position, boolean isChecked) {
+            mCheckStates.put(position, isChecked);
+            System.out.println("hello...........");
+            notifyDataSetChanged();
+        }
+
+        public void toggle(int position) {
+            setChecked(position, !isChecked(position));
+        }
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView,
+                                     boolean isChecked) {
+            // TODO Auto-generated method stub
+
+            mCheckStates.put((Integer) buttonView.getTag(), isChecked);
+        }
+    }
+
+
+/*
     private RecyclerView recyclerViewLista;
     RecyclerAdapter recyclerAdapter;
     private List<Contacto> listaContactos = new ArrayList<Contacto>();
@@ -21,6 +169,11 @@ public class MainActivity extends AppCompatActivity {
             ContactsContract.Contacts.DISPLAY_NAME,
             ContactsContract.Contacts.HAS_PHONE_NUMBER,
     };
+
+    List<String> name1 = new ArrayList<String>();
+    List<String> phno1 = new ArrayList<String>();
+    MyAdapter ma ;
+    Button select;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,4 +214,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+*/
 }
